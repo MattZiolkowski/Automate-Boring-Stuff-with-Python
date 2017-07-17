49cdd069213243b9c51ea9d5228de0f9142d05ea
Matt <matt.ziolkowski88@gmail.com>
2017-07-17 17:52:33 +0200

    Update 2048auto.py

--- 2048auto.py
+++ 2048auto.py
@@ -1,5 +1,5 @@
 #! python3
-# GmailLogIn.py - Program logs into gmail account using Selenium in Firefox browser.
+# 2048auto.py - Program automatically plays the 2048 game for you by opening the site and hitting 100 times up, right, down and left.
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,9 +9,9 @@ import time
 
 browser = webdriver.Firefox()
 
-browser.get('http:2048game.com')
+browser.get('http:2048game.com')  # Game's website.
 
-time.sleep(2)
+time.sleep(2)  # Allows browser to open a game.
 
 board = browser.find_element_by_css_selector('body')
 #Find email log in field by its id.
